c19a30b312e11cc27477434c13c2433c38d61ec3
Rick Qui <rick.qui@netscout.com>
2017-04-02 09:13:47 +0100

    Spark 2.1 examples

--- src/main/scala/org/test/spark2/perf/TungstenPerf.scala
+++ src/main/scala/org/test/spark2/perf/TungstenPerf.scala
@@ -1,14 +1,16 @@
 package org.test.spark2.perf
+
 import scala.math._
 import org.apache.spark.sql.SparkSession
 /**
- * TungstenPerformace demonstrates a low memory usage by using Tungsten Engine. 
+ * The TungstenPerf demonstrates a low memory usage by using Tungsten Engine. 
  * The memory usages can be viewed in SparkUI http://localhost:10200/storage/
  */
-object TungstenPerformace {
+object TungstenPerf {
   def main(arg: Array[String]) {
+    
+    val spark = SparkSession.builder().master("local[2]").config("spark.ui.port", 10200).appName("TungstenPerf").getOrCreate()
 
-    val spark = SparkSession.builder().master("local[2]").config("spark.ui.port", 10200).appName("TungstenPerformace").getOrCreate()
     import spark.implicits._
 
     val million = spark.sparkContext.parallelize(0 until math.pow(10, 6).toInt)
@@ -17,8 +19,12 @@ object TungstenPerformace {
     million.cache.count
     //Dataset
     million.toDS().cache.count
-
-    Thread.sleep(1200000)
+    
+    Thread.sleep(60000)
+    
+    Thread.sleep(5000)
+    
+ 
     spark.stop
   }
 }
